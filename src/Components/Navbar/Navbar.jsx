b3f0d1d5679e8b11f777abbd68a3115452d367df
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-10-27 14:42:18 +0545

    delivery

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,19 +4,36 @@ import { ImFlag } from "react-icons/im";
 import { TbWorld } from "react-icons/tb";
 import { MdOutlinePeopleAlt } from "react-icons/md";
 
-
 const Navbar = () => {
   const [activeCategory, setActiveCategory] = useState(null);
 
   const categories = [
-    { name: "Deliver to: ", icon: <ImFlag />, items: ["Phones", "Laptops", "Cameras"] },
-    { name: "World", icon: <TbWorld />, items: ["Men", "Women", "Kids"] },
-    { name: "Home ", icon:<MdOutlinePeopleAlt/>, items: ["Furniture", "Decor", "Kitchen"] },
+    {
+      name: "Deliver to",
+      icon: <ImFlag />,
+      text: "NP",
+      items: (
+        <div>
+          <h1>Specify your location</h1>
+          <p>Shipping options and fees vary based on your location</p>
+          <button onClick={() => console.log("Login button clicked")}>Sign in to add address</button>
+          <u>or</u>
+          <input type="text" placeholder="" />
+          <input type="text" placeholder="please input the right zip/postal code." />
+          <button>save</button>
+        </div>
+      ),
+    },
+    { icon: <TbWorld />, items: ["Men", "Women", "Kids"] },
+    { icon: <MdOutlinePeopleAlt />, items: ["Furniture", "Decor", "Kitchen"] },
   ];
 
   const handleCategoryClick = (index) => {
     setActiveCategory(activeCategory === index ? null : index);
   };
+  const handleDropdownClick = (event)=>{
+    event.stopPropagation();
+  }
 
   return (
     <nav className="navbar">
@@ -29,12 +46,14 @@ const Navbar = () => {
       <ul className="navbar-categories">
         {categories.map((category, index) => (
           <li key={index} onClick={() => handleCategoryClick(index)}>
-            {category.icon} {category.name}
+            {category.icon} {category.name} {category.text}
             {activeCategory === index && (
-              <ul className="dropdown">
-                {category.items.map((item, itemIndex) => (
-                  <li key={itemIndex}>{item}</li>
-                ))}
+              <ul className="dropdown" onClick={handleDropdownClick}>
+                {Array.isArray(category.items) ? (
+                  category.items.map((item, itemIndex) => <li key={itemIndex}>{item}</li>)
+                ) : (
+                  <li>{category.items}</li>
+                )}
               </ul>
             )}
           </li>
