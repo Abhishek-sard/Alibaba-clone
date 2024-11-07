f7269a6b2d7abe272841e1f749f31709f7c5f3d8
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-11-07 14:22:53 +0545

    image

--- Frontend/src/Components/Text/Text.jsx
+++ Frontend/src/Components/Text/Text.jsx
@@ -15,8 +15,31 @@ export const Text = () => {
           well-established enterprise looking for tools and <br />
           solutions for more complex orders.
         </h5>
-        <h6><u>Learn More</u></h6>
+        <h6>
+          <u>Learn More</u>
+        </h6>
       </div>
+      <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     </div>
   );
 };
