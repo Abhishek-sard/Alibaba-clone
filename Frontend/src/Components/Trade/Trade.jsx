923dff5fac9650e477a52b7ac008f6ee423fb10e
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-11-10 17:59:47 +0545

    images

--- Frontend/src/Components/Trade/Trade.jsx
+++ Frontend/src/Components/Trade/Trade.jsx
@@ -4,47 +4,51 @@ import "./Trade.css";
 export const Trade = () => {
   return (
     <div className="trade-container">
+      {/* <h1>Empowering business through global trade</h1>
+      <h5>
+        Alibaba.com offers one-stop B2B trading solutions for global small and
+        medium-sized businesses, <br />
+        empowering them to transform through digital trade, grasp opportunities,
+        and accelerate growth <br />
+        internationally.
+      </h5>  */}
+
       {/* Left Side: First Large Card */}
       <div className="card-large">
-        <img src="/man.jpg.png" className="card-img" alt="Card Image" />
+        <img src="/man.jpg" className="card-img" alt="Card Image" />
         <div className="card-img-overlay">
           <h5 className="card-title">OUR MISSION</h5>
-          <p className="card-text">
+          <h3 className="card-text">
             Make it easy to do business <br />
             anywhere.
-          </p>
+          </h3>
         </div>
       </div>
 
       {/* Right Side: Stacked Smaller Cards */}
       <div className="card-small-container">
         <div className="card-small">
-          <img src="/earth.jpg.png" className="card-img" alt="Card Image" />
+          <img src="/earth.jpg" className="card-img" alt="Card Image" />
           <div className="card-img-overlay">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">OUR LOCATIONS</h5>
+            <h3 className="card-text">We have teams around the world.</h3>
             <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p className="card-text">
-              <small>Last updated 3 mins ago</small>
+              <small>
+                Hangzhou, China Paris, France Munich, Germany Tokyo, Japa Seoul,
+                Korea London, Uk
+              </small>
             </p>
           </div>
         </div>
 
         <div className="card-small">
-          <img src="/wind.jpg.png" className="card-img" alt="Card Image" />
+          <img src="/wind.jpg" className="card-img" alt="Card Image" />
           <div className="card-img-overlay">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p className="card-text">
-              <small>Last updated 3 mins ago</small>
-            </p>
+            <h5 className="card-title">OUR ESG PROMISES</h5>
+            <h3 className="card-text">
+              Responsible technology. <br />
+              Sustainable future.
+            </h3>
           </div>
         </div>
       </div>
