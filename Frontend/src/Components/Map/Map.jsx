b93fb69eef462ee30c927626559671c25ba31a28
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-11-11 20:10:38 +0545

    footer

--- Frontend/src/Components/Map/Map.jsx
+++ Frontend/src/Components/Map/Map.jsx
@@ -39,6 +39,55 @@ export const Map = () => {
         </div>
       </div>
       <hr className="divider" />
+      <div className="footer">
+        <table>
+          <thead>
+            <tr>
+                <th>Get support</th>
+                <th>Payments and Protections</th>
+                <th>Source on Alibaba.com</th>
+                <th>Sell on Alibaba.com</th>
+                <th>Get to know us</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td>Help center</td>
+                <td>Safe and easy payments</td>
+                <td>Request for Quotation</td>
+                <td>Start selling</td>
+                <td>About Alibaba.com</td>
+            </tr>
+            <tr>
+                <td>Live chat</td>
+                <td>Money-back policy</td>
+                <td>Membership program</td>
+                <td>Seller Central</td>
+                <td>Corporate responsibility</td>
+            </tr>
+            <tr>
+                <td>Check order status</td>
+                <td>On-time shipping</td>
+                <td>Alibaba.com Logistics</td>
+                <td>Become a Verified Supplier</td>
+                <td>News center</td>
+            </tr>
+            <tr>
+                <td>Refunds</td>
+                <td>After-sales protections</td>
+                <td>Sales tax and VAT</td>
+                <td>Partnerships</td>
+                <td>Careers</td>
+            </tr>
+            <tr>
+                <td>Report abuse</td>
+                <td>Product monitoring services</td>
+                <td>Alibaba.com Reads</td>
+                <td>Download the app for suppliers</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
