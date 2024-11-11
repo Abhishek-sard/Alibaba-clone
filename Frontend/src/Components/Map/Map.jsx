a46d1eb78113054fdee0d4df82e891f041091884
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-11-11 15:14:07 +0545

    map-com

--- Frontend/src/Components/Map/Map.jsx
+++ Frontend/src/Components/Map/Map.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import './Map.css'
+import React from "react";
+import "./Map.css";
 
 export const Map = () => {
   return (
-    <div>Map</div>
-  )
-}
-export default Map
+    <div className="map-text">
+      <h2>Find suppliers by country or region</h2>
+      <h6>
+        <u>View More</u>
+      </h6>
+      <div className="map-country">
+        <div className="images-country">
+          <img src="/japan.jpg" alt="" />
+          <p>Japan</p>
+        </div>
+        <div className="images-country">
+          <img src="/Thailand.jpg" alt="" />
+          <p>Thailand</p>
+        </div>
+        <div className="images-country">
+          <img src="/turkey.jpg" alt="" />
+          <p>Turkiye</p>
+        </div>
+        <div className="images-country">
+          <img src="/malaysia.jpg" alt="" />
+          <p>Malaysia</p>
+        </div>
+        <div className="images-country">
+          <img src="/vietnam.jpg" alt="" />
+          <p>Vietnam</p>
+        </div>
+        <div className="images-country">
+          <img src="/russia.jpg" alt="" />
+          <p>Russia</p>
+        </div>
+        <div className="images-country">
+          <img src="/southkorea.jpg" alt="" />
+          <p>South Korea</p>
+        </div>
+      </div>
+      <hr className="divider" />
+    </div>
+  );
+};
+export default Map;
