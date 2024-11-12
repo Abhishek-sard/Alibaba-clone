2847e0021e004d557fcbe3a5a985d3b8e51a2a23
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-11-12 21:09:05 +0545

    images added

--- Frontend/src/Components/Map/Map.jsx
+++ Frontend/src/Components/Map/Map.jsx
@@ -43,50 +43,62 @@ export const Map = () => {
         <table>
           <thead>
             <tr>
-                <th>Get support</th>
-                <th>Payments and Protections</th>
-                <th>Source on Alibaba.com</th>
-                <th>Sell on Alibaba.com</th>
-                <th>Get to know us</th>
+              <th>Get support</th>
+              <th>Payments and Protections</th>
+              <th>Source on Alibaba.com</th>
+              <th>Sell on Alibaba.com</th>
+              <th>Get to know us</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-                <td>Help center</td>
-                <td>Safe and easy payments</td>
-                <td>Request for Quotation</td>
-                <td>Start selling</td>
-                <td>About Alibaba.com</td>
+              <td>Help center</td>
+              <td>Safe and easy payments</td>
+              <td>Request for Quotation</td>
+              <td>Start selling</td>
+              <td>About Alibaba.com</td>
             </tr>
             <tr>
-                <td>Live chat</td>
-                <td>Money-back policy</td>
-                <td>Membership program</td>
-                <td>Seller Central</td>
-                <td>Corporate responsibility</td>
+              <td>Live chat</td>
+              <td>Money-back policy</td>
+              <td>Membership program</td>
+              <td>Seller Central</td>
+              <td>Corporate responsibility</td>
             </tr>
             <tr>
-                <td>Check order status</td>
-                <td>On-time shipping</td>
-                <td>Alibaba.com Logistics</td>
-                <td>Become a Verified Supplier</td>
-                <td>News center</td>
+              <td>Check order status</td>
+              <td>On-time shipping</td>
+              <td>Alibaba.com Logistics</td>
+              <td>Become a Verified Supplier</td>
+              <td>News center</td>
             </tr>
             <tr>
-                <td>Refunds</td>
-                <td>After-sales protections</td>
-                <td>Sales tax and VAT</td>
-                <td>Partnerships</td>
-                <td>Careers</td>
+              <td>Refunds</td>
+              <td>After-sales protections</td>
+              <td>Sales tax and VAT</td>
+              <td>Partnerships</td>
+              <td>Careers</td>
             </tr>
             <tr>
-                <td>Report abuse</td>
-                <td>Product monitoring services</td>
-                <td>Alibaba.com Reads</td>
-                <td>Download the app for suppliers</td>
+              <td>Report abuse</td>
+              <td>Product monitoring services</td>
+              <td>Alibaba.com Reads</td>
+              <td>Download the app for suppliers</td>
             </tr>
           </tbody>
         </table>
+      </div>
+      <div className="images-add">
+        <div className="images-flex">
+          <img src="/id.png" alt="" height="40px"  width="40px" />
+          <img src="/dss.png" alt=""height="40px"  width="40px" />
+          <img src="/ssl.png" alt="" height="40px"  width="40px" />
+          <img src="/verisign.png" alt="" height="40px"  width="40px" />
+          <img src="/visa.png" alt="" height="40px"  width="40px"/>
+          <img src="/visaa.png" alt=""height="40px"  width="40px" />
+          <img src="/color.png" alt="" height="40px"  width="40px" />
+          <img src="/tt.png" alt="" height="40px"  width="40px"/>
+        </div>
       </div>
     </div>
   );
